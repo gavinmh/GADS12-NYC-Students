fe74921a25daaa12f4126e6f20e19a92c933265e
Steven Benario <sbenario@gmail.com>
2014-08-12 18:46:15 -0400

    Removing my API key from the code

--- lab_submissions/lab01/steven_benario/nytimes_vizualization.py
+++ lab_submissions/lab01/steven_benario/nytimes_vizualization.py
@@ -1,29 +1,25 @@
 #! /usr/bin/env python
 import sys
+import os
 import json
 import requests
 import matplotlib.pyplot as plt
 
 #this file will graph the number of articles written about a topic by year
 
-
 def querySingleYear(term, year):
 	#input: query term, year
 	#output: number of results
 
 	#this function calls the NYT api to get how many responses there are in a year
-	#r = requests.get('http://api.nytimes.com/svc/search/v2/articlesearch.json?q=gaza&begin_date=20130101&end_date=20131231&api-key=f24f8ba49940b4a690c352e10bd1e387:2:69647018')
-	
-    # MAKE SURE TO INSERT API KEY OR UNCOMMENT THIS LINE
-    #mykey = 'f24f8ba49940b4a690c352e10bd1e387:2:69647018'
+	#r = requests.get('http://api.nytimes.com/svc/search/v2/articlesearch.json?q=computers&begin_date=20130101&end_date=20131231&api-key=MYAPIKEYGOESHERE')
     
-
-	requestString = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=%s&begin_date=%d0101&end_date=%d1231&api-key=%s' % (term, year, year, mykey)
-
-	myResponse = requests.get(requestString).json()
-	numHits = myResponse['response']['meta']['hits']
-
-	return numHits
+    requestString = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=%s&begin_date=%d0101&end_date=%d1231&api-key=%s' % (term, year, year, MyNYTKey)
+    
+    myResponse = requests.get(requestString).json()
+    numHits = myResponse['response']['meta']['hits']
+    
+    return numHits
 
 def generateListing(term, startyear):
 	#This function generates the (DATASTRUCT) that has year, and # responses, for all years from 
@@ -47,6 +43,15 @@ def main():
 
 	print "We're going to get some information on the frequency of search terms in NYTimes Articles"
 	print
+
+	# We're using this section to prevent needing to store the API key in the code
+	global MyNYTKey
+	MyNYTKey = os.environ.get("NYTIMES_APIKEY")
+	if MyNYTKey == None:
+	    print 'Error! Please enter (export) your API key to the variable "NYTIMES_APIKEY"'
+	    exit(2)
+
+
 	searchterm = raw_input("What term would you like to search for? ")
 	startyear = raw_input("And what year would you like to start from? ")
 
